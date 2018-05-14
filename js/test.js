603f61798b29e2200ed34dc39e40028a581ebfbd
kawhileo <kawhileo@sina.com>
2018-05-14 14:40:41 +0800

    样式图片

---
+++ js/test.js
@@ -0,0 +1,11 @@
+function Index(node){
+    this.node=node;
+    this.init();
+}
+Index.prototype.init=function(){
+    var self=this;
+    this.node=[];
+    Array.prototype.slice.call(self.node,0).array.forEach(function(item,index) {
+        
+    });
+}
